5a54a29f4d18fc9f564e8a9114b0be0c9ffdac0d
Matheus Galdino <matheus.kho@gmail.com>
2023-05-16 22:37:57 -0300

    feat: adding interactivity and validation to signin form

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { SecondaryButton } from "../../components/Button/Button";
 import { Button } from "../../components/Button/Button";
 import "./SignIn.scss";
-import { signIn } from "../../services/authService";
+import { SignInCredentials, signIn } from "../../services/authService";
 import { redirect } from "react-router-dom";
 
+
 export default function SignIn() {
+    const [userCredentials, setUserCredentials] = useState<SignInCredentials>({ email: '', password: '' });
+
     function handleSubmit(event: React.FormEvent) {
-        event.preventDefault()
-        const credentials = {"email":"matheus@gaa", "password": "umasenhaaa"}
+        event.preventDefault();
+        const credentials: SignInCredentials = { ...userCredentials };
 
-        const validacoes = true;
-        if (validacoes) {
-
+        if (userCredentials.email && userCredentials.password) {
             signIn(credentials).then(response => {
-                console.log(response.data)
-                localStorage.setItem('credentials', response.data)
+                console.log(response.data);
+                localStorage.setItem('credentials', JSON.stringify(response.data));
                 redirect('/');
-            })
+            });
+        }else{
+            console.log('invalido');
+            
         }
+    }
+
+    function handleFormChange(event: React.FormEvent<HTMLInputElement>) {
+        const { name, value } = event.currentTarget;
+
+        const newUserCredentials: SignInCredentials = {
+            ...userCredentials,
+            [name]: value,
+        };
+
+        setUserCredentials(newUserCredentials);
     }
 
     return <div className="form-container">
@@ -27,10 +42,10 @@ export default function SignIn() {
             <div className="inputs-container">
 
                 <label htmlFor="email">E-mail</label>
-                <input type="email" name="email" />
+                <input type="email" name="email" onChange={handleFormChange}/>
 
                 <label htmlFor="password">Senha</label>
-                <input type="password" name="password" />
+                <input type="password" name="password" onChange={handleFormChange}/>
                 <p className="forget-password">
                     Esqueceu a senha?
                 </p>
---
+++ src/types/credentials.ts
@@ -0,0 +1,19 @@
+export type Authorization = {
+    token: string;
+    type: "bearer";
+};
+
+export type User = {
+    id: string;
+    name: string;
+    email: string;
+    email_verified_at: string;
+    created_at: string;
+    updated_at: string;
+};
+
+export type SignInCredentials = {
+    email: string;
+    password: string;
+};
+
