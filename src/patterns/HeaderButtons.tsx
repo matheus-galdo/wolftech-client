f989e7015140d9503af0359b8314c5c85d55db9b
Matheus Galdino <matheus.kho@gmail.com>
2023-05-22 23:59:38 -0300

    style: aligning header buttons

--- src/patterns/HeaderButtons.tsx
+++ src/patterns/HeaderButtons.tsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { MdShoppingCart, MdAccountCircle, } from 'react-icons/md';
+import { MdShoppingCart, MdAccountCircle, MdKeyboardArrowDown} from 'react-icons/md';
 import { AuthContext } from '../context/AuthContext';
 import './headerButtons.scss';
 
@@ -23,12 +23,13 @@ export default function HeaderButtons() {
         </>}
 
         {authContext?.user && <button className="btn-nav material-icons">
+            <MdKeyboardArrowDown className="dropdown-icon"/>
             {authContext?.user?.name}
-            <MdAccountCircle />
+            <MdAccountCircle className="btn-icon"/>
         </button>}
 
         <button className="btn-nav material-icons cart-btn">
-            <MdShoppingCart />
+            <MdShoppingCart className="btn-icon"/>
             <span className="carrinho-counter">
                 10
             </span>
