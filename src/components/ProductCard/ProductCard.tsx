b3556a11c95bb09e4eab6c13da184f734ca62add
Matheus Galdino <matheus.kho@gmail.com>
2023-05-30 23:46:38 -0300

    feat: basic validation to ensure that adding to cart is only available for signed users

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,26 +1,45 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Card.scss';
 import formatProductPrice from './ProductPrice';
 import { Product } from '../../types/Products';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
 
 export type ProductProps = {
     product: Product
 };
 
 export default function ProductCard({ product }: ProductProps) {
+    const navigate = useNavigate();
+    const authContext = useContext(AuthContext);
 
-    return <Link className="product-card" to={`produtos/${product.id}`}>
-        <div className='product-card-wrapper'>
-            <div className="image-container">
-                <img src={product.imageUrl} alt={product.name} />
-            </div>
-            <div className="card-details">
-                <h1 className="name">{product.name}</h1>
-                <p className="price">{formatProductPrice(product.price)}</p>
-                <div className="buttons">
-                    <button>Adicionar ao carrinho</button>
+    //TODO: create generic and reusable function to add product to cart
+    function addToCart(ev: React.MouseEvent, product: any) {
+        ev.preventDefault();
+        
+        if (!authContext?.userIsLoggedIn) {
+            const error = new Response("Unauthenticated", { status: 401 });
+            return navigate("/error", {state: error});
+        }
+
+        //TODO: send post request to shopping-cart
+        navigate("/carrinho");
+    }
+
+    return <div className="product-card">
+        <Link to={`produtos/${product.id}`}>
+            <div className='product-card-wrapper'>
+                <div className="image-container">
+                    <img src={product.imageUrl} alt={product.name} />
+                </div>
+                <div className="card-details">
+                    <h1 className="name">{product.name}</h1>
+                    <p className="price">{formatProductPrice(product.price)}</p>
                 </div>
             </div>
+        </Link >
+        <div className="buttons">
+            <button onClick={(ev) => addToCart(ev, {})}>Adicionar ao carrinho</button>
         </div>
-    </Link >;
+    </div>;
 }
---
+++ src/pages/ErrorPage/Error.tsx
@@ -0,0 +1,15 @@
+import { isRouteErrorResponse, useLocation, useRouteError } from "react-router-dom";
+
+export default function Error() {
+    const error = useRouteError();
+    const location = useLocation();
+    console.log({ location });
+
+    return <div>
+        {isRouteErrorResponse(error) ? <>
+            Ocorreu um erro: {error.status}
+        </> : <>
+            Occorreu um erro inesperado
+        </>}
+    </div>;
+}
